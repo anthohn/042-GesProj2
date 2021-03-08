41456d39da0f3b7545c0981699bfc7590ee58a78
KillianGood <61836895+KillianGood@users.noreply.github.com>
2021-03-08 16:22:04 +0100

    create temp folder
    temp folder

---
+++ temps-file/index.php
@@ -0,0 +1,26 @@
+<html>
+	<head> 
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/ossn.default.css" />
+	</head>
+
+	<body>
+		<div class="widget ">
+		<div class="widget-heading">Connexion</div>
+		<div class="widget-contents">
+			<form id="login" action="login.php" class="form" method="post"  enctype='multipart/form-data'><fieldset>  
+        <div>
+			<label>Nom d'utilisateur</label>
+    		<input type="text" name="username" />
+		</div>
+		<div>
+			<label>Mot de passe</label>
+    		<input type="password" name="password" />
+		</div>
+		<div>		
+    		<input type="submit" value="Connexion" class="btn btn-primary"/>
+   		</div>
+		</fieldset></form>	</div>
+		</div>             
+	</body>
+</html>
