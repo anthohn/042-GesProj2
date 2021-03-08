36654f1a3d86857d623e853e61faafad97b6d263
KillianGood <61836895+KillianGood@users.noreply.github.com>
2021-03-08 15:22:20 +0100

    Deleted temp file
    deleted temp file I use to try to connect the db

--- temp-kilgood/connect_to_database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassowrd = "root";
-    $dbName = "kilgood";
-
-    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassowrd, $dbName);
-?>
