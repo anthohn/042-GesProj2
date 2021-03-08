a70d58c2a1301bae6fb23aabbde14b35eca8d522
Julienbarnabé Cartier <julcartier@etmlnet.local>
2021-03-08 08:57:24 +0100

    Page à propos maj

--- code/src/php/about.php
+++ code/src/php/about.php
@@ -12,8 +12,8 @@
 
 		<!-- Logo onglet-->
 		<link rel="icon" href="../../userContent/logoOnglet.png" />
-		<title>Accueil</title>
 		<link href="../../resources/css/style.css" rel="stylesheet" type="text/css" />
+		<title>À propos</title>
 		<script src="../js/toggleMenu.js" defer></script>
 	</head>	
 	<body>	
@@ -27,28 +27,35 @@
 			<!-- Bar de navigation -->
 			<div class="leftnav">
 				<ul class="navBarLink">	
-					<li class="logo"><a href="home.php">Oto</a></li>				
-					<li><a href="home.php"><img src="../../userContent/logo/home.svg" alt="">Accueil</a></li>
-					<li><a href="research.php"><img src="../../userContent/logo/search.svg" alt="">Recherche</a></li>
+					<li class="logo"><a href="accueil.php">Oto</a></li>				
+					<li><a href="accueil.php"><img src="../../userContent/logo/home.svg" alt="">Accueil</a></li>
+					<li><a href="recherche.php"><img src="../../userContent/logo/search.svg" alt="">Recherche</a></li>
 					<li class="espace"><a href="library.php"><img src="../../userContent/logo/library.svg" alt="">Bibliothèque</a></li>
-					<li class="border"><a href="artists.php">Artistes</a></li>
+					<li class="border"><a href="artistes.php">Artistes</a></li>
 					<li><a href="playlists.php">Playlists</a></li>
-					<li><a href="alltitle.php">Tous les titres</a></li>
-					<li><a href="likedtitle.php">Titres likés</a></li>
-					<li class="bottom"><a id="active2" href="about.php">about</a></li>
+					<li class="bottom"><a id="active2" href="about.php">À propos</a></li>
 				</ul>
 			</div>
 		</div>
+		<style>
+			body {
+			background-image: url(' ../../userContent/about/image.png');
+			}
+		</style>
 		<div class="contenu">
 			<div class="contenutext">
 				<p>Oto est un site web développé par une petite équipe d’informaticien de l’ETML en apprentisssge. 
-				Il permet notament, grâce plusieurs bases de données, de pouvoir gérer différentes liste de lecture 
-				ainsi que de rajouter ou supprimer des artistes. Grâce à ce site, tu peux sans souci répertorier les 
-				différents morceaux préférer de tes artistes préféré dans différentes listes de lecture, afin de mettre de 
+				Il permet notament, grâce à base de données, de pouvoir gérer différentes listes de lecture 
+				ainsi que de rajouter ou supprimer des artistes. Sur ce site, tu peux sans souci répertorier les 
+				différents morceaux de tes artistes préférés, afin de mettre de 
 				l’ordre dans tes sons favoris !</p>
 			</div>
 		</div>
-
+		
+		<div class="resContenu">
+		<p>Nos réseaux sociaux :</p>
+		</div>
+		<a class="resSVG" href=https://instagram.com><img src="../../userContent/logo/insta.svg" alt="Insta" target="_blank"></a>
 		<footer>
 		</footer>
 	</body>
