db3bce415d3dc07d26b3374542b544dbe2778b1c
Anthony Höhn <anthonyhohn@gmail.com>
2021-03-17 11:20:31 +0100

    test

--- code/src/php/404.php
+++ code/src/php/404.php
@@ -3,7 +3,7 @@
 <div class="maintest">
     <div class="error">
         <center><h1>Erreur 404</h1></center>
-        <p>Vous semblez vous être perdu..</p>
+        <p>Vous semblez vous êtree perdu..</p>
     </div>
 </div>
 
