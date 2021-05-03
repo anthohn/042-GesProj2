122a355358c48bb298c5b7b7279cd389cc26c9a6
Younes Sayeh <younes.sayeh@eduvaud.ch>
2021-05-03 16:22:13 +0200

    #6#11 login responsive, research playlist (not finish)

--- code/src/php/template/header.php
+++ code/src/php/template/header.php
@@ -4,6 +4,7 @@ require "lib/util.php";
 require "lib/db.class.php";
 require "config/dbconfig.cfg";
 $DB = new DB (Config::$host, Config::$username, Config::$password, Config::$database);
+$activePage = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -27,15 +28,49 @@ $DB = new DB (Config::$host, Config::$username, Config::$password, Config::$data
 			<!-- Bar de navigation -->
 			<div class="leftnav">
 				<ul class="navBarLink">
-					<li class="logo"><a id="active" href="home.php">Oto</a></li>
-					<li class="home"><a href="home.php"><img src="../../userContent/logo/home.svg" alt="">Accueil</a></li>
-					<li class="research"><a href="research.php"><img src="../../userContent/logo/search.svg" alt="">Recherche</a></li>
-					<!-- <li ><a href="library.php"><img src="../../userContent/logo/library.svg" alt="">Bibliothèque</a></li> -->
-					<li class="allArtists"><a href="allartists.php">Artistes</a></li>
-					<li class="playlists"><a href="playlists.php">Playlists</a></li>
-					<li class="allTitle"><a href="alltitle.php">Tous les titres</a></li>
-					<li class="likedTitle"><a href="likedtitle.php">Titres likés</a></li>
-					<li class="about"><a href="about.php">À propos</a></li>
+					<li class="logo"><a href="home.php">Oto</a></li>
+
+					<?php if($activePage == 'home.php') : ?>
+						<li class="home"><a id="active" href="home.php"><img src="../../userContent/logo/home.svg" alt="">Accueil</a></li>
+					<?php else : ?>
+						<li class="home"><a href="home.php"><img src="../../userContent/logo/home.svg" alt="">Accueil</a></li>
+					<?php endif; ?>
+
+					<?php if($activePage == 'research.php') : ?>
+						<li class="research"><a id="active" href="research.php"><img src="../../userContent/logo/search.svg" alt="">Recherche</a></li>
+					<?php else : ?>
+						<li class="research"><a href="research.php"><img src="../../userContent/logo/search.svg" alt="">Recherche</a></li>
+					<?php endif; ?>
+
+					<?php if($activePage == 'allartists.php') : ?>
+						<li class="allArtists"><a id="active" href="allartists.php">Artistes</a></li>
+					<?php else : ?>
+						<li class="allArtists"><a href="allartists.php">Artistes</a></li>
+					<?php endif; ?>
+
+					<?php if($activePage == 'playlists.php') : ?>
+						<li class="playlists"><a id="active" href="playlists.php">Playlists</a></li>
+					<?php else : ?>
+						<li class="playlists"><a href="playlists.php">Playlists</a></li>
+					<?php endif; ?>
+
+					<?php if($activePage == 'alltitle.php') : ?>
+						<li class="allTitle"><a id="active" href="alltitle.php">Tous les titres</a></li>
+					<?php else : ?>
+						<li class="allTitle"><a href="alltitle.php">Tous les titres</a></li>
+					<?php endif; ?>
+
+					<?php if($activePage == 'likedtitle.php') : ?>
+						<li class="likedTitle"><a id="active" href="likedtitle.php">Titres likés</a></li>
+					<?php else : ?>
+						<li class="likedTitle"><a href="likedtitle.php">Titres likés</a></li>
+					<?php endif; ?>
+
+					<?php if($activePage == 'about.php') : ?>
+						<li class="about"><a id="active" href="about.php">À propos</a></li>
+					<?php else : ?>
+						<li class="about"><a href="about.php">À propos</a></li>
+					<?php endif; ?>
 					<div class="login-container">
                    		<?php if(!isLogged()): ?>
 						<form method="post" action="home.php" id="loginForm">
@@ -46,8 +81,6 @@ $DB = new DB (Config::$host, Config::$username, Config::$password, Config::$data
 							</div>
 						</form>
 						<?php else: ?>
-
-							
 							<div class="notlog">
 								<p><?= 'Bonjour ' . $_SESSION['username']; ?></p>
 								<a href="home.php?auth=logout">Se deconnecter</a>
