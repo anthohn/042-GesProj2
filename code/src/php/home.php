8342ae7e4fe0ce5334413f118f1ee7ac4ed24a6d
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2021-05-03 18:14:37 +0200

    correction home page

--- code/src/php/home.php
+++ code/src/php/home.php
@@ -23,19 +23,12 @@ $musics = $DB->getAllTitle();
 </div>
 <div class="ACmainBlock">	
 	<?php foreach ( $musics as $music): ?>
-	<?php $idMusic = $music['idMusic']; ?>
-	<?php $links = $DB->getLinkEachMusics($idMusic); ?>		
-	<?php foreach ($links as $link): ?>	
 		<div class="ACblock">
-			<a href="<?= $link["linLink"]; ?>"target="_blank"><?= $link["typLiens"]; ?>
 
 			<h1><?= $music["musName"]; ?></h1>
 			<img src="../../userContent/img/music/<?= $music["idMusic"] ?>.jpg" alt="">
 			<p>Musique</p>
 		</div>
-		
-		</a>
-		<?php endforeach; ?>	
 	<?php endforeach ?>
 </div>
 <?php require "template/footer.php" ?>
