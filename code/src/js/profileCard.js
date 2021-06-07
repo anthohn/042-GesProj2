509efaea384fead26db26ac6906351c6bf4871f4
Younes Sayeh <younes.sayeh@eduvaud.ch>
2021-06-07 16:09:44 +0200

    #28 Optimisation du code JS/HTML/CSS avec une boucle for

--- code/src/js/profileCard.js
+++ code/src/js/profileCard.js
@@ -2,88 +2,26 @@
 //	Author      : Younes Sayeh
 //	Date        : 27.05.2021
 //	Description : Profile Card
-//  Boucle for incremente 4 fois et concaténer variable d'incrémentation 
-//  selectionnez la div par rapport a un id
 
-const clc = document.querySelector(".cancel");
-const arr = document.querySelector(".arrContainer");
-const leftContainer = document.querySelector(".leftContainer");
+for(let i = 1; i <= 4; i++) {
+    let clc = document.querySelector("#member"+i + " .cancel");
+    let arr = document.querySelector("#member"+i + " .arrContainer");
+    let leftContainer = document.querySelector("#member"+i + " .leftContainer");
 
-const clc2 = document.querySelector(".cancel2");
-const arr2 = document.querySelector(".arrContainer2");
-const leftContainer2 = document.querySelector(".leftContainer2");
-
-const clc3 = document.querySelector(".cancel3");
-const arr3 = document.querySelector(".arrContainer3");
-const leftContainer3 = document.querySelector(".leftContainer3");
-
-const clc4 = document.querySelector(".cancel4");
-const arr4 = document.querySelector(".arrContainer4");
-const leftContainer4 = document.querySelector(".leftContainer4");
-
-arr.addEventListener("click", ()=>{
-    arr.classList.add("activeArr");
-    if(leftContainer.classList.contains("off")) {
-        leftContainer.classList.remove("off");
-        leftContainer.classList.add("active");
-    }
-});
-
-clc.addEventListener("click", ()=>{
-    arr.classList.remove("activeArr");
-    if(leftContainer.classList.contains("active")) {
-        leftContainer.classList.remove("active");
-        leftContainer.classList.add("off");
-    }
-});
-
-
-arr2.addEventListener("click", ()=>{
-    arr2.classList.add("activeArr2");
-    if(leftContainer2.classList.contains("off2")) {
-        leftContainer2.classList.remove("off2");
-        leftContainer2.classList.add("active2");
-    }
-});
-
-clc2.addEventListener("click", ()=>{
-    arr2.classList.remove("activeArr2");
-    if(leftContainer2.classList.contains("active2")) {
-        leftContainer2.classList.remove("active2");
-        leftContainer2.classList.add("off2");
-    }
-});
-
-
-arr3.addEventListener("click", ()=>{
-    arr3.classList.add("activeArr3");
-    if(leftContainer3.classList.contains("off3")) {
-        leftContainer3.classList.remove("off3");
-        leftContainer3.classList.add("active3");
-    }
-});
-
-clc3.addEventListener("click", ()=>{
-    arr3.classList.remove("activeArr3");
-    if(leftContainer3.classList.contains("active3")) {
-        leftContainer3.classList.remove("active3");
-        leftContainer3.classList.add("off3");
-    }
-});
-
-
-arr4.addEventListener("click", ()=>{
-    arr4.classList.add("activeArr4");
-    if(leftContainer4.classList.contains("off4")) {
-        leftContainer4.classList.remove("off4");
-        leftContainer4.classList.add("active4");
-    }
-});
-
-clc4.addEventListener("click", ()=>{
-    arr4.classList.remove("activeArr4");
-    if(leftContainer4.classList.contains("active4")) {
-        leftContainer4.classList.remove("active4");
-        leftContainer4.classList.add("off4");
-    }
-});
+    arr.addEventListener("click", ()=>{
+        arr.classList.add("activeArr");
+        if(leftContainer.classList.contains("off")) {
+            leftContainer.classList.remove("off");
+            leftContainer.classList.add("active");
+        }
+    });
+    
+    clc.addEventListener("click", ()=>{
+        arr.classList.remove("activeArr");
+        if(leftContainer.classList.contains("active")) {
+            leftContainer.classList.remove("active");
+            leftContainer.classList.add("off");
+        }
+    });
+    
+}
